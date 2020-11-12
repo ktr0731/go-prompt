c538e3314b80661459992c6769d02efec6cbfee7
Antony Dovgal <tony@daylessday.org>
2020-11-12 18:51:22 +0300

    fix build with pkg/term 1.1.0

--- internal/term/raw.go
+++ internal/term/raw.go
@@ -3,9 +3,8 @@
 package term
 
 import (
-	"syscall"
-
 	"github.com/pkg/term/termios"
+	"golang.org/x/sys/unix"
 )
 
 // SetRaw put terminal into a raw mode
@@ -15,12 +14,12 @@ func SetRaw(fd int) error {
 		return err
 	}
 
-	n.Iflag &^= syscall.IGNBRK | syscall.BRKINT | syscall.PARMRK |
-		syscall.ISTRIP | syscall.INLCR | syscall.IGNCR |
-		syscall.ICRNL | syscall.IXON
-	n.Lflag &^= syscall.ECHO | syscall.ICANON | syscall.IEXTEN | syscall.ISIG | syscall.ECHONL
-	n.Cflag &^= syscall.CSIZE | syscall.PARENB
-	n.Cc[syscall.VMIN] = 1
-	n.Cc[syscall.VTIME] = 0
-	return termios.Tcsetattr(uintptr(fd), termios.TCSANOW, (*syscall.Termios)(&n))
+	n.Iflag &^= unix.IGNBRK | unix.BRKINT | unix.PARMRK |
+		unix.ISTRIP | unix.INLCR | unix.IGNCR |
+		unix.ICRNL | unix.IXON
+	n.Lflag &^= unix.ECHO | unix.ICANON | unix.IEXTEN | unix.ISIG | unix.ECHONL
+	n.Cflag &^= unix.CSIZE | unix.PARENB
+	n.Cc[unix.VMIN] = 1
+	n.Cc[unix.VTIME] = 0
+	return termios.Tcsetattr(uintptr(fd), termios.TCSANOW, (*unix.Termios)(&n))
 }
